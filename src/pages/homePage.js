5ef76cf1db3ec53aaa132ab1ff2b9b795d9a67db
Vitor Wogel <vitorandre26@gmail.com>
2022-10-14 20:23:19 +0100

    Refactor: Template list page; HTTP API calls centralized.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,11 +1,11 @@
-import Header from "../components/headerMovieList";
-import FilterCard from "../components/filterMoviesCard";
-import MovieList from "../components/movieList";
-import React, { useState, useEffect } from "react";  // Changed
-import Grid from "@mui/material/Grid";
+import React, { useState, useEffect } from "react";
+import { getMovies } from "../api/tmdb-api";
+import PageTemplate from "../components/templateMovieListPage";
 
-const MovieListPage = (props) => {
+const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
+  const favourites = movies.filter((m) => m.favourite);
+  localStorage.setItem("favourites", JSON.stringify(favourites));
 
   const addToFavourites = (movieId) => {
     const updatedMovies = movies.map((m) =>
@@ -15,32 +15,18 @@ const MovieListPage = (props) => {
   };
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        return json.results;
-      })
-      .then((movies) => {
-        setMovies(movies);
-      });
-    //  eslint-disable-next-line react-hooks/exhaustive-deps
+    getMovies().then(movies => {
+      setMovies(movies);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <Grid container sx={{ padding: "20px" }}>
-      <Grid item xs={12}>
-        <Header title={"Home Page"} />
-      </Grid>
-      <Grid item container spacing={5}>
-        <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
-          <FilterCard />
-        </Grid>
-        <MovieList movies={movies} selectFavourite={addToFavourites} />
-      </Grid>
-    </Grid>
+    <PageTemplate
+      title="Discover Movies"
+      movies={movies}
+      selectFavourite={addToFavourites}
+    />
   );
 };
-export default MovieListPage;
+export default HomePage;
