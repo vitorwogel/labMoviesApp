503b9ecdf5d9d2f76ac76e3cf0fe65eb9d386ec2
Vitor Wogel <vitorandre26@gmail.com>
2022-11-03 01:31:16 +0000

    Fix upcoming movies page.

--- src/pages/upcomingDetails.js
+++ src/pages/upcomingDetails.js
@@ -2,11 +2,25 @@ import React from "react";
 import { useParams } from 'react-router-dom';
 import MovieDetails from "../components/movieDetails/";
 import PageTemplate from "../components/templateMoviePage";
-import useUpcoming from "../hooks/useUpcoming";
+import { getUpcoming } from '../api/tmdb-api'
+import { useQuery } from "react-query";
+import Spinner from '../components/spinner'
 
 const MoviePage = (props) => {
   const { id } = useParams();
-  const [movie] = useUpcoming(id);
+  
+  const { data: movie, error, isLoading, isError } = useQuery(
+    ["movie", { id: id }],
+    getUpcoming
+  );
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
 
   return (
     <>
--- src/pages/upcomingMovies.js
+++ src/pages/upcomingMovies.js
@@ -1,31 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { getUpcoming } from "../api/tmdb-api";
 import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
 
 const UpcomingMovies = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favourites = movies.filter((m) => m.favourite);
-  localStorage.setItem("favourites", JSON.stringify(favourites));
+  
+  const {  data, error, isLoading, isError }  = useQuery('discover', getUpcoming)
 
-  const addToFavourites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favourite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
+  if (isLoading) {
+    return <Spinner />
+  }
 
-  useEffect(() => {
-    getUpcoming().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favourites = movies.filter(m => m.favourite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
+  const addToFavourites = (movieId) => true 
 
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
-      selectFavourite={addToFavourites}
+      action={(movie) => {
+        return <AddToFavouritesIcon movie={movie} />
+      }}
     />
   );
 };
