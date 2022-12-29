fb83dd407e1aa22dccb4517f7d56865eb193fa85
Vitor Wogel <vitorandre26@gmail.com>
2022-12-29 19:59:15 +0000

    home and upcoming coming from movies-api (backend)

--- src/api/movie-api.js
+++ src/api/movie-api.js
@@ -20,9 +20,23 @@ export const signup = (username, password) => {
 
 export const getMovies = () => {
     return fetch(
+       '/api/movies'
+    ).then(res => res.json());
+};
+
+export const getUpcoming = () => {
+    return fetch(
+        '/api/movies/tmdb/upcoming'
+    ).then(res => res.json())
+}
+
+/*
+export const getMovies = () => {
+    return fetch(
        '/api/movies',{headers: {
          'Authorization': window.localStorage.getItem('token')
       }
     }
     ).then(res => res.json());
 };
+*/
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { getMovies } from "../api/tmdb-api";
+import { getMovies } from "../api/movie-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
